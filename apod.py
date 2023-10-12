9dacbedcebe47edcbfe27b890f457d5ca63c2da1
!interesting <92435626+StupidProgramming@users.noreply.github.com>
2023-10-12 20:23:43 +0200

    Updated apod.py
    Updated the script for using the C:\Users\x\APODimgs folder

--- apod.py
+++ apod.py
@@ -1,12 +1,16 @@
 import requests
 import os
 
-api_key = "9YcMRAW7oBmRAc0Di6dwgy5Ho4nrnyLanbtm5jg6"
+# Code by StupidProgramming for setting the wallpaper to the current Astronomy Picture Of the Day
+# 12 October 2023
+
+api_key = "9YcMRAW7oBmRAc0Di6dwgy5Ho4nrnyLanbtm5jg6" # nasa api key
 APOD_URL = "https://api.nasa.gov/planetary/apod?api_key=" + api_key
 EPIC_URL = "https://api.nasa.gov/EPIC/api/natural?api_key=" + api_key
 apod_data = requests.get(APOD_URL).json()
 
 # get url of APOD_image, if APOD is video then use EPIC picture.
+# EPIC = Earth Polychromatic Imaging Camera
 # apod == 1: image is apod, apod == 0: image is epic
 
 if apod_data["media_type"] == "image":
@@ -25,19 +29,18 @@ else:
 
 # make path so we can download to it, only needed first time
 
-os.system("mkdir c:\\Users\\" + os.getlogin() + "\\EPICimgs")
+os.system("mkdir c:\\Users\\" + os.getlogin() + "\\APODimgs")
 
 # download the file to imagename
 
-os.system("powershell -c Invoke-WebRequest -Uri '" + url + "' -OutFile 'c:\\Users\\" + os.getlogin() + "\\EPICimgs\\" + imagename + "\'")
+os.system("powershell -c Invoke-WebRequest -Uri '" + url + "' -OutFile 'c:\\Users\\" + os.getlogin() + "\\APODimgs\\" + imagename + "\'")
 
 bmpname = imagename.replace("png", "bmp").replace("jpg", "bmp") # make name to save bmp to
 
 # convert to bmp
 
-os.system("powershell -c [Reflection.Assembly]::LoadWithPartialName(\\\"System.Windows.Forms\\\"); $convertfile = new-object System.Drawing.Bitmap(\\\"c:\\Users\\" + os.getlogin() + "\\EPICimgs\\" + imagename + "\\\"); $convertfile.Save(\\\"c:\\Users\\" + os.getlogin() + "\\EPICimgs\\" + bmpname + "\\\", \\\"bmp\\\")")
+os.system("powershell -c [Reflection.Assembly]::LoadWithPartialName(\\\"System.Windows.Forms\\\"); $convertfile = new-object System.Drawing.Bitmap(\\\"c:\\Users\\" + os.getlogin() + "\\APODimgs\\" + imagename + "\\\"); $convertfile.Save(\\\"c:\\Users\\" + os.getlogin() + "\\APODimgs\\" + bmpname + "\\\", \\\"bmp\\\")")
 
 
-# if we want to set it as wallpaper, uncomment this:
-
-os.system("reg add \"HKEY_CURRENT_USER\Control Panel\Desktop\" /v Wallpaper /t REG_SZ /d \"c:\\Users\\" + os.getlogin() + "\\EPICimgs\\" + bmpname + "\" /f | RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters")
+# if you dont want to set it as wallpaper, comment this:
+os.system("reg add \"HKEY_CURRENT_USER\Control Panel\Desktop\" /v Wallpaper /t REG_SZ /d \"c:\\Users\\" + os.getlogin() + "\\APODimgs\\" + bmpname + "\" /f | RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters")
